0767ae7293521547c53fb3513e57e3347c638ab3
Freddy Sanchez <fredsv511@gmail.com>
2025-03-23 11:05:15 -0500

    RESTAURAR REVIT API BASICO
    Se restauro el código original

--- JS/Cursos_Individuales.js
+++ JS/Cursos_Individuales.js
@@ -1,35 +1,47 @@
-document.addEventListener('DOMContentLoaded', () => {
-  // Resaltado dinámico de temas
-  document.querySelectorAll('.nav-item').forEach(item => {
-      item.addEventListener('click', function (e) {
-          e.preventDefault();
 
-          // Remueve la clase 'active' de todos los elementos
-          document.querySelectorAll('.nav-item').forEach(navItem => {
-              navItem.classList.remove('active');
-          });
-
-          // Agrega la clase 'active' al elemento clicado
-          this.classList.add('active');
-
-          // Carga el video correspondiente
-          const videoUrl = this.getAttribute('href');
-          document.querySelector('iframe[name="cuadro"]').src = videoUrl;
-      });
-  });
-
-  // Menú responsive
-  const menuBtn = document.querySelector('.menu-icon');
-  const menu = document.querySelector('.navigation');
-
-  menuBtn.addEventListener('click', () => {
-      menu.classList.toggle('show');
-  });
-
-  // Cierra el menú al hacer clic fuera
-  document.addEventListener('click', (e) => {
-      if (!menu.contains(e.target) && !menuBtn.contains(e.target)) {
-          menu.classList.remove('show');
+jQuery('document').ready(function($) {
+    var menuBtn = $('.menu-icon'),
+        menu = $('.navigation ul');
+  
+    menuBtn.click(function(event) {
+      event.stopPropagation(); // Agregar esta línea para detener la propagación del evento
+      if (menu.hasClass('show')) {
+        menu.removeClass('show');
+      } else {
+        menu.addClass('show');
       }
+    });
+  
+    menu.click(function(event) {
+      event.stopPropagation(); // Agregar esta línea para detener la propagación del evento
+    });
+  
+    $(document).click(function() {
+      if (menu.hasClass('show')) {
+        menu.removeClass('show');
+      }
+    });
   });
-});
+  
+  
+  
+  //INICIAR AUTOMATICAMENTE CON UN CLICK
+  var enlace = document.getElementById("miEnlace"); // Selecciona el enlace
+  enlace.click(); // Simula un clic en el enlace
+  
+  
+  
+  //Cambiar color de elemento seleccionado
+  function showIframe() {
+      var navItems = document.querySelectorAll('.nav-item');
+      for (var i = 0; i < navItems.length; i++) {
+        navItems[i].classList.remove('active');
+      }
+      event.currentTarget.parentElement.classList.add('active');
+    }
+  
+    function loadPage(url) {
+      document.getElementById("iframe-enlace").src = url;
+    }
+  
+  
